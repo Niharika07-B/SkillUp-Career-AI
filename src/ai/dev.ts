cf394bc616af43cac9701c159d1e3034e573c958
Niharika Bandaru <niharika.bandaru5002@gmail.com>
2025-08-12 21:05:21 +0530

    Delete all files

--- src/ai/dev.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-config();
- 
-import '@/ai/flows/gemini-chatbot.ts';
-import '@/ai/flows/ats-resume-scanner.ts';
-import '@/ai/flows/resume-generator-flow.ts';
-import '@/ai/flows/interview-flow.ts';
